16deec168c0ae84b3ed5045444db4317db24ada0
Əbil Abilli <ebilebilli3@gmail.com>
2025-06-25 02:29:38 +0400

    Category serializer created

--- movie_website/categories/serializers/category_serializer.py
+++ movie_website/categories/serializers/category_serializer.py
@@ -1,2 +1,19 @@
-# from rest_framework import serializers
+from rest_framework import serializers
 
+from models.category import Category
+
+
+__all__ = [
+    'CategorySerializer',
+]
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    movie_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+    def get_movie_count(self, obj):
+        return obj.movies.count()
