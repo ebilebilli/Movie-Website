73a7f0956cb0b6a412e09665b37cda6fd51bd9d3
Əbil Abilli <ebilebilli3@gmail.com>
2025-06-24 22:16:49 +0400

    CustomerUser model created

--- movie_website/users/models.py
+++ movie_website/users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class CustomeUser(models.Model):
+    email = models.EmailField(
+        unique=True,
+        verbose_name='Email'  
+        )
+    username = models.CharField(
+        unique=True,
+        max_length=20,
+        verbose_name='Username'
+        )
+    birthday = models.DateField(
+        verbose_name='Birthday'
+        )
+    bio = models.TextField(
+        max_length=250,
+        verbose_name='Bio'
+    )
+     
