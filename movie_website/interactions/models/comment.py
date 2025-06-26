657117d3fb6dbd4dbb6e18ed67ec1ccbe4e58884
Əbil Abilli <ebilebilli3@gmail.com>
2025-06-26 16:00:49 +0400

    Comment model created

---
+++ movie_website/interactions/models/comment.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+
+from users.models.user import CustomerUser
+from movies.models.movie import Movie
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        CustomerUser, 
+        on_delete=models.CASCADE,
+        verbose_name='User',
+        related_name='comments'
+        )
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+        verbose_name='Movie',
+        related_name='comments'
+    )
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='Parent',
+        related_name='replies'
+    )
+
+    text = models.TextField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural  = 'Comments'
+    
+    def clean(self):
+        if self.parent and self.parent.movie != self.movie:
+            raise ValidationError('Reply comment must be for the same movie as the parent comment')
+
+    def __str__(self):
+        if self.parent:
+            return f'{self.user} replied to {self.parent} in movie: {self.movie}'
+        
+        return f'{self.user} add comment in movie: {self.movie}'
+    
