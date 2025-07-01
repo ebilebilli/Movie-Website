b58f6c27a5062018e132206255bdcda01b1ac761
Əbil Abilli <ebilebilli3@gmail.com>
2025-07-01 23:08:47 +0400

    Small problem solved in filter for movie serializer

--- movie_website/movies/serializers/movie_serializer.py
+++ movie_website/movies/serializers/movie_serializer.py
@@ -9,12 +9,11 @@ __all__ = [
 ]
 
 class MovieSerializer(serializers.ModelSerializer):
-    slug = serializers.SlugField(read_only=True)
     poster = serializers.ImageField(use_url=True)
 
     class Meta:
         model = Movie
-        fields = ['title', 'categories',  'rating', 'poster', 'slug']
+        fields = ['title', 'categories',  'rating', 'poster']
 
 
 class MovieDetailSerializer(serializers.ModelSerializer):
