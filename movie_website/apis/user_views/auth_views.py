0834c5ada943668355bf8fb2fddafd60c93eb81d
Əbil Abilli <ebilebilli3@gmail.com>
2025-07-02 14:09:17 +0400

    Login view created

--- movie_website/apis/user_views/auth_views.py
+++ movie_website/apis/user_views/auth_views.py
@@ -34,3 +34,26 @@ class RegisterAPIView(APIView):
                 },status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginAPIView(APIView):
+    permission_classes = [AllowAny]
+    http_method_names = ['post']
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            refresh = RefreshToken.for_user(user)
+            tokens = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token)
+            }
+
+            return  Response({
+                'message': 'Successfully logged in',
+                'tokens': tokens
+                },status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
