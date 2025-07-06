0e2ee61f94c670eb7d9f9aa5039630530de1e01c
Əbil Abilli <ebilebilli3@gmail.com>
2025-07-06 15:08:41 +0400

    I am continue integrate openai to project

--- movie_website/apis/ai_apis/ai_views.py
+++ movie_website/apis/ai_apis/ai_views.py
@@ -3,7 +3,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
-from rest_framework.permissions import AllowAny
 
 from movies.models.movie import Movie
 from users.models.user import CustomerUser
@@ -11,7 +10,7 @@ from ai.tasks import generate_ai_response_task
 
 
 class MovieQueryRequestToAIAPIView(APIView):
-    authentication_classes = [AllowAny]
+    permission_classes = [AllowAny]
     http_method_names = ['post']
 
     def post(self, request):
@@ -25,6 +24,6 @@ class MovieQueryRequestToAIAPIView(APIView):
         
         else:
             message = f'Sorry, there is not such movie on our website'
-        
-        generate_ai_response_task(question=question, message=message)
-    
+
+        ai_response = generate_ai_response_task(question=question, message=message)
+        return Response(ai_response, status=status.HTTP_200_OK)
